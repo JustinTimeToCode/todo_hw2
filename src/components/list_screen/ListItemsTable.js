be87ccc975535b84a6565eed9f6687363f2c337c
codartist <40900036+codartist@users.noreply.github.com>
2019-10-08 19:50:03 -0400

    Fixed the font and completed item screen controls.

--- src/components/list_screen/ListItemsTable.js
+++ src/components/list_screen/ListItemsTable.js
@@ -2,19 +2,35 @@ import React, { Component } from 'react'
 import ListItemCard from './ListItemCard'
 
 export class ListItemsTable extends Component {
+    
+    constructor(props){
+        super(props);
+
+        this.state = {
+            listToEdit: props.todoList
+        }
+    }
+
     render() {
         return (
             <div id="list_items_container">
-                <div className="list_item_task_header">Task</div>
-                <div className="list_item_due_date_header">Due Date</div>
-                <div className="list_item_status_header">Status</div>
+                <div className = "list_item_header_card">
+                    <div className="list_item_task_header" >Task</div>
+                    <div className="list_item_due_date_header">Due Date</div>
+                    <div className="list_item_status_header">Status</div>
+                </div>    
                 {
-                    this.props.todoList.items.map((todoItem)=>(
+                    this.props.todoList.items.map((todoItem)=>( //todoListItem (Object)
                         <ListItemCard 
                             key={todoItem.key}
-                            listItem={todoItem} />
+                            todoList={this.props.todoList}
+                            listItem={todoItem}
+                            loadListItem={this.props.loadListItem.bind(this, todoItem)} />
                     ))
                 }
+                <div onClick = {this.props.goListItem} className = 'list_item_add_card'>
+                    <strong> + </strong>
+                </div>
             </div>
         )
     }
